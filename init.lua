811ad5179d0eab494ce412d29c65ecba10162d6b
aiirononeko <aiirononeko@gmail.com>
2022-12-05 23:17:35 +0900

    set up lueTree

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require "plugins"
 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
@@ -11,11 +13,19 @@ vim.api.nvim_exec("highlight SignColumn ctermbg=black", false)
 vim.wo.number = true
 vim.wo.cursorline = true
 vim.cmd 'set clipboard+=unnamedplus'
+vim.cmd("colorscheme gotham")
+vim.opt.termguicolors = true
 
--- ファイルを開いた時に、カーソルの場所を復元する
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
 	pattern = { "*" },
 	callback = function()
 		vim.api.nvim_exec('silent! normal! g`"zv', false)
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "plugins.lua" },
+  command = "PackerCompile",
+})
+
+require("nvim-tree").setup();
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,12 @@
 vim.cmd[[packadd packer.nvim]]
 
 require"packer".startup(function()
-    use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
+	use "whatyouhide/vim-gotham"
+	use {
+  	'nvim-tree/nvim-tree.lua',
+  	requires = {
+    	'nvim-tree/nvim-web-devicons',
+  	},
+	}
 end)
